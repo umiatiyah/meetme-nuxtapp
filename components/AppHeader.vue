6566785bd6d283d692a043972c92ff1d8abaa391
umiatiyah <umiatiyah97@gmail.com>
2023-08-17 01:24:43 +0700

    fixing src to load image

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,107 +1,133 @@
 <template>
-    <!-- Header Area wrapper Starts -->
-    <header id="header-wrap">
-      <!-- Navbar Start -->
-      <nav class="navbar navbar-expand-lg fixed-top scrolling-navbar indigo">
-        <div class="container">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-              <span class="icon-menu"></span>
-              <span class="icon-menu"></span>
-              <span class="icon-menu"></span>
-            </button>
-            <a href="/" class="navbar-brand"><img src="img/logo.png" alt=""></a>
-          </div>
-          <div class="collapse navbar-collapse" id="main-navbar">
-            <ul class="onepage-nev navbar-nav mr-auto w-100 justify-content-end clearfix">
-              <li class="nav-item active">
-                <a class="nav-link" href="#hero-area">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#about">
-                  About
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#resume">
-                  Resume
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#services">
-                  Services
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+  <!-- Header Area wrapper Starts -->
+  <header id="header-wrap">
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg fixed-top scrolling-navbar indigo">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#main-navbar"
+            aria-controls="main-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+            <span class="icon-menu"></span>
+            <span class="icon-menu"></span>
+            <span class="icon-menu"></span>
+          </button>
+          <a href="/" class="navbar-brand"
+            ><img src="/img/logo.png" alt=""
+          /></a>
         </div>
+        <div class="collapse navbar-collapse" id="main-navbar">
+          <ul
+            class="onepage-nev navbar-nav mr-auto w-100 justify-content-end clearfix"
+          >
+            <li class="nav-item active">
+              <a class="nav-link" href="#hero-area"> Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about"> About </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#resume"> Resume </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#services"> Services </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact"> Contact </a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-        <!-- Mobile Menu Start -->
-        <ul class="onepage-nev mobile-menu">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">about</a>
-          </li>
-          <li>
-            <a href="#resume">resume</a>
-          </li>
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-        <!-- Mobile Menu End -->
-      </nav>
-      <!-- Navbar End -->
+      <!-- Mobile Menu Start -->
+      <ul class="onepage-nev mobile-menu">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">about</a>
+        </li>
+        <li>
+          <a href="#resume">resume</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <!-- Mobile Menu End -->
+    </nav>
+    <!-- Navbar End -->
 
-      <!-- Hero Area Start -->
-      <div id="hero-area" class="hero-area-bg">
-        <div class="overlay"></div>
-        <div class="container">
-         
-          <div class="row">
-            <div class="col-md-12 col-sm-12 text-center">
-              <div class="contents">
-                <h5 class="script-font wow fadeInUp" data-wow-delay="0.2s">Hi This is</h5>
-                <h2 class="head-title wow fadeInUp" data-wow-delay="0.4s">Umi Atiyah</h2>
-                <p class="script-font wow fadeInUp" data-wow-delay="0.6s">seekers of knowledge</p>
-                <ul class="social-icon wow fadeInUp" data-wow-delay="0.8s">
-                  <li>
-                    <a class="instagram" href="https://www.instagram.com/umiiatiyah/" target="_blank"><i class="icon-social-instagram"></i></a>
-                  </li>
-                  <li>
-                    <a class="linkedin" href="https://www.linkedin.com/in/umi-atiyah-703151159/" target="_blank"><i class="icon-social-linkedin"></i></a>
-                  </li>
-                  <li>
-                    <a class="github" href="https://github.com/umiatiyah" target="_blank"><i class="icon-social-github"></i></a>
-                  </li>
-                  <li>
-                    <a class="google" href="https://www.youtube.com/channel/UC-RxLeD_iRJj5W40NGKVF1w?view_as=subscriber" target="_blank"><i class="icon-social-youtube"></i></a>
-                  </li>
-                </ul>
-                <div class="header-button wow fadeInUp" data-wow-delay="1s">
-                  <a href="#about" class="btn btn-common">Meet mee</a>
-                </div>
+    <!-- Hero Area Start -->
+    <div id="hero-area" class="hero-area-bg">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 text-center">
+            <div class="contents">
+              <h5 class="script-font wow fadeInUp" data-wow-delay="0.2s">
+                Hi This is
+              </h5>
+              <h2 class="head-title wow fadeInUp" data-wow-delay="0.4s">
+                Umi Atiyah
+              </h2>
+              <p class="script-font wow fadeInUp" data-wow-delay="0.6s">
+                seekers of knowledge
+              </p>
+              <ul class="social-icon wow fadeInUp" data-wow-delay="0.8s">
+                <li>
+                  <a
+                    class="instagram"
+                    href="https://www.instagram.com/umiiatiyah/"
+                    target="_blank"
+                    ><i class="icon-social-instagram"></i
+                  ></a>
+                </li>
+                <li>
+                  <a
+                    class="linkedin"
+                    href="https://www.linkedin.com/in/umi-atiyah-703151159/"
+                    target="_blank"
+                    ><i class="icon-social-linkedin"></i
+                  ></a>
+                </li>
+                <li>
+                  <a
+                    class="github"
+                    href="https://github.com/umiatiyah"
+                    target="_blank"
+                    ><i class="icon-social-github"></i
+                  ></a>
+                </li>
+                <li>
+                  <a
+                    class="google"
+                    href="https://www.youtube.com/channel/UC-RxLeD_iRJj5W40NGKVF1w?view_as=subscriber"
+                    target="_blank"
+                    ><i class="icon-social-youtube"></i
+                  ></a>
+                </li>
+              </ul>
+              <div class="header-button wow fadeInUp" data-wow-delay="1s">
+                <a href="#about" class="btn btn-common">Meet mee</a>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <!-- Hero Area End -->
-
-    </header>
-    <!-- Header Area wrapper End -->    
+    </div>
+    <!-- Hero Area End -->
+  </header>
+  <!-- Header Area wrapper End -->
 </template>
